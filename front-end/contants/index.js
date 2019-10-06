8909338fb7cdf53d1e87df41eceb3558b27e021e
Ahmet Özdemir <ahmet.ozdemir@obss.com.tr>
2019-10-06 18:26:37 +0300

    homepage implementation

---
+++ front-end/contants/index.js
@@ -0,0 +1,109 @@
+import _ from 'lodash';
+
+export const BOTTOM_NAV_TABS = [
+  {
+    key: 'homepage',
+    icon: 'home',
+    label: 'Anasayfa',
+    barColor: '#f9f9f9',
+    pressColor: '#f9f9f9',
+    labelStyle: {
+      color: '#504746'
+    }
+  },
+  {
+    key: 'shelters',
+    icon: 'barcode',
+    label: 'Barınaklar',
+    barColor: '#f9f9f9',
+    pressColor: '#f9f9f9',
+    labelStyle: {
+      color: '#504746'
+    }
+  },
+  {
+    key: 'add',
+    icon: 'add',
+    label: '',
+    barColor: '#d6402c',
+    pressColor: '#f9f9f9',
+    labelStyle: {
+      color: '#504746'
+    }
+  },
+  {
+    key: 'blog',
+    icon: 'paper',
+    label: 'Blog',
+    barColor: '#f9f9f9',
+    pressColor: '#f9f9f9',
+    labelStyle: {
+      color: '#504746'
+    }
+  },
+  {
+    key: 'profile',
+    icon: 'person',
+    label: 'Profil',
+    barColor: '#f9f9f9',
+    pressColor: '#f9f9f9',
+    labelStyle: {
+      color: '#504746'
+    }
+  }
+];
+
+export const STORIES = [
+  {
+    image: 'https://picsum.photos/100/100',
+    text: _.uniqueId('user_')
+  },
+  {
+    image: 'https://picsum.photos/100/100',
+    text: _.uniqueId('user_')
+  },
+  {
+    image: 'https://picsum.photos/100/100',
+    text: _.uniqueId('user_')
+  },
+  {
+    image: 'https://picsum.photos/100/100',
+    text: _.uniqueId('user_')
+  },
+  {
+    image: 'https://picsum.photos/100/100',
+    text: _.uniqueId('user_')
+  },
+  {
+    image: 'https://picsum.photos/100/100',
+    text: _.uniqueId('user_')
+  },
+  {
+    image: 'https://picsum.photos/100/100',
+    text: _.uniqueId('user_')
+  },
+  {
+    image: 'https://picsum.photos/100/100',
+    text: _.uniqueId('user_')
+  },
+  {
+    image: 'https://picsum.photos/100/100',
+    text: _.uniqueId('user_')
+  },
+  {
+    image: 'https://picsum.photos/100/100',
+    text: _.uniqueId('user_')
+  },
+  {
+    image: 'https://picsum.photos/100/100',
+    text: _.uniqueId('user_')
+  },
+  {
+    image: 'https://picsum.photos/100/100',
+    text: _.uniqueId('user_')
+  },
+  {
+    image: 'https://picsum.photos/100/100',
+    text: _.uniqueId('user_')
+  }
+];
