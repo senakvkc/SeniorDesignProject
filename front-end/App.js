af345713810ecd20b9e90432ea7ec75068a3b431
Ahmet Özdemir <ahmet.ozdemir@obss.com.tr>
2019-10-06 18:25:58 +0300

    App.js status bar hidden

--- front-end/App.js
+++ front-end/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import Homepage from './components/common/Homepage';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <>
+      <StatusBar hidden />
+      <Homepage />
+    </>
   );
 }
 
@@ -14,6 +16,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
