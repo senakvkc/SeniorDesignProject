1031060c1e14a4f7da9e95304dfd84ee6b467511
Ahmet Özdemir <ahmet.ozdemir@obss.com.tr>
2020-05-09 22:53:08 +0300

    profile screen improvmeents

--- front-end/scenes/Profile/ProfileScreen.js
+++ front-end/scenes/Profile/ProfileScreen.js
@@ -1,15 +1,16 @@
 import React from "react";
-import { Text, StyleSheet, Image, TouchableOpacity, View, Dimensions } from "react-native";
+import { Text, StyleSheet, Image, TouchableOpacity, View, Dimensions, Share } from "react-native";
 import { Icon } from 'react-native-elements';
 import { withTranslation } from 'react-i18next';
 import _ from 'lodash';
+import PropTypes from 'prop-types';
 
 import MainButton from '../../components/common/MainButton';
 import { USER_TOKEN } from '../../constants';
 
 const { width: screenWidth, height: screenHeight } = Dimensions.get('window');
 
-const ProfileScreen = () => {
+const ProfileScreen = ({ t, navigation }) => {
 
   const actions = [
     {
@@ -18,7 +19,8 @@ const ProfileScreen = () => {
       text: 'Konuşma Başlat',
       onPress: () => startConversation(),
       iconColor: '#8ED5A6',
-      iconBgColor: '#F0FFF5'
+      iconBgColor: '#F0FFF5',
+      hasBorder: true
     },
     {
       id: _.uniqueId('action_'),
@@ -26,7 +28,8 @@ const ProfileScreen = () => {
       text: 'Evcil Hayvanlarım',
       onPress: () => startConversation(),
       iconColor: '#A4BEEA',
-      iconBgColor: '#F3F9FE'
+      iconBgColor: '#F3F9FE',
+      hasBorder: true
     },
     {
       id: _.uniqueId('action_'),
@@ -34,7 +37,8 @@ const ProfileScreen = () => {
       text: 'Beğendiklerim',
       onPress: () => startConversation(),
       iconColor: '#FEA195',
-      iconBgColor: '#FFF5F4'
+      iconBgColor: '#FFF5F4',
+      hasBorder: true
     },
     {
       id: _.uniqueId('action_'),
@@ -42,7 +46,8 @@ const ProfileScreen = () => {
       text: 'Blog Yazılarım',
       onPress: () => startConversation(),
       iconColor: '#C38ED5',
-      iconBgColor: '#F0F0FF'
+      iconBgColor: '#F0F0FF',
+      hasBorder: true
     },
     {
       id: _.uniqueId('action_'),
@@ -50,7 +55,8 @@ const ProfileScreen = () => {
       text: 'Profilim',
       onPress: () => startConversation(),
       iconColor: '#95BFFE',
-      iconBgColor: '#DFECFE'
+      iconBgColor: '#DFECFE',
+      hasBorder: false
     },
   ];
 
@@ -66,9 +72,9 @@ const ProfileScreen = () => {
     console.log("start");
   }
 
-  const renderAction = ({ id, icon, text, onPress, iconColor, iconBgColor }) => {
+  const renderAction = ({ id, icon, text, onPress, iconColor, iconBgColor, hasBorder }) => {
     return (
-      <TouchableOpacity key={id} onPress={onPress} style={styles.actionContainer} activeOpacity={0.8}>
+      <TouchableOpacity key={id} onPress={onPress} style={[styles.actionContainer, { borderBottomWidth: hasBorder ? 1 : 0, paddingBottom: hasBorder ? 18 : 0 }]} activeOpacity={0.8}>
         <View style={styles.actionLeft}>
           <Icon type="material" name={icon} size={18} color={iconColor} containerStyle={getIconStyles(iconBgColor)} />
           <Text style={styles.actionText}>
@@ -80,27 +86,49 @@ const ProfileScreen = () => {
     )
   };
 
-  return (
-    <View style={styles.container}>
-      <View style={styles.topSection}>
-        <Image source={{ uri: 'https://placedog.net/120/120' }} style={styles.profileImage} />
-        <Text style={styles.profileName}>
-          Ezgi İmamoğlu
-        </Text>
-        <Text style={styles.profileRole}>
-          Hayvansever
-        </Text>
-        <Icon type="feather" name="check-circle" size={24} color="#FEA195" containerStyle={styles.profileIcon} />
-      </View>
-      <View style={styles.actionsSection}>
-        {_.map(actions, action => renderAction({...action}))}
-      </View>
+  
+  const reportProfile = () => {
+    console.log("report");
+  }
 
-    </View>
+  return (
+    <>
+      <TouchableOpacity onPress={() => navigation.goBack()}>
+        <Icon name="chevron-left" type="material" size={20} containerStyle={styles.goBackButton} color="#000" />
+      </TouchableOpacity>
+      <TouchableOpacity onPress={reportProfile} activeOpacity={0} style={styles.reportButton}>
+        <Icon type="feather" name="alert-circle" size={24} color="#fff" />
+      </TouchableOpacity>
+      <View style={styles.container}>
+        <View style={styles.topSection}>
+          <Image source={{ uri: 'https://placedog.net/120/120' }} style={styles.profileImage} />
+          <Text style={styles.profileName}>
+            Ezgi İmamoğlu
+          </Text>
+          <Text style={styles.profileRole}>
+            Hayvansever
+          </Text>
+          <Icon type="feather" name="check-circle" size={24} color="#FEA195" containerStyle={styles.profileIcon} />
+        </View>
+        <View style={styles.actionsSection}>
+          {_.map(actions, action => renderAction({...action}))}
+        </View>
+      </View>
+    </>
   )
 };
 
 const styles = StyleSheet.create({
+  goBackButton: {
+    position: 'absolute',
+    top: 20,
+    left: 20,
+  },
+  reportButton: {
+    position: 'absolute',
+    right: 24,
+    top: 24
+  },
   container: {
     flex: 1,
     flexDirection: 'column'
@@ -150,7 +178,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    paddingVertical: 18,
+    paddingTop: 18,
     borderBottomColor: '#f4f4f1',
     borderBottomWidth: 1
   },
@@ -169,5 +197,10 @@ const styles = StyleSheet.create({
     paddingRight: 18,
   }
 });
+
+ProfileScreen.propTypes = {
+  t: PropTypes.func.isRequired,
+  navigation: PropTypes.shape({}).isRequired
+}
 
 export default withTranslation()(ProfileScreen);
